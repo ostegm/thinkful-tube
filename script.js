13b766b7580ad7639ef18c6aa8adcfb7a3d8b561
Otto <o.stegmaier@gmail.com>
2018-01-29 19:41:56 -0800

    Working api call

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const ZEEKEY = 'AIzaSyAkkPLJ01lsIR_BUSaqt05A6QseNQHI05M';
+const ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
 
+function getData(query_phrase, callback) {
+  const query = {
+    part: 'snippet',
+    q: query_phrase,
+    key: ZEEKEY,
+  }
+  console.log(`Preparing to send query: ${query}`);
+  $.getJSON(ENDPOINT, query, callback)
+}
+
+function displayResults(data) {
+  console.log(data);
+}
 
 
 function watchSubmit() {
@@ -6,6 +21,9 @@ function watchSubmit() {
     event.preventDefault();
     const q = $('#search-term').val()
     $('#search-term').val("")
+
+    getData(q, displayResults)
+
 	})
 }
 
